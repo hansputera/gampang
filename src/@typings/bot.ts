4c5f6065ba0d477d33400aabd9208045a3001dc5
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-08-13 02:46:40 +0000

    feat(typings.ClientEvents): add message
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/@typings/bot.ts
+++ src/@typings/bot.ts
@@ -1,4 +1,5 @@
 import makeWASocket, { AuthenticationState } from '@adiwajshing/baileys';
+import { Context } from '../structures';
 
 export type MakeWaSocketType = ReturnType<typeof makeWASocket>;
 export interface RawClient extends MakeWaSocketType {
@@ -12,4 +13,5 @@ export interface ClientEvents {
   'qr': (code: string) => Promise<void> | void;
   'ready': () => Promise<void> | void;
   'logout': () => Promise<void> | void;
+  'message': (context: Context) => Promise<void | void>;
 }
