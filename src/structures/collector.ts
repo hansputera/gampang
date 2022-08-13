e4fe7d050305651bcaf68166b0fc0f0647d81566
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-08-13 06:18:36 +0000

    feat(structures): add MessageCollector
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

---
+++ src/structures/collector.ts
@@ -0,0 +1,107 @@
+import type { Context } from './context';
+import type { CollectorOptions } from '../@typings';
+import EventEmitter from 'events';
+import { Client, CommandClient } from '../bot';
+import type { CommandContext } from './context.command';
+
+/**
+ * @class MessageCollector
+ */
+export class MessageCollector<C extends Client | CommandClient> {
+  public runner?: NodeJS.Timeout;
+  /**
+   * Message collector session key
+   */
+  private key!: string;
+  /**
+   * Validate a message function
+   */
+  public validate!: CollectorOptions<C>['validation'];
+
+  /**
+   * @param {Context} ctx - A context of message
+   */
+  constructor(
+    public ctx: C extends Client ? Context : CommandContext,
+    private options: CollectorOptions<C> = {
+      time: 30 * 1000,
+      max: 3,
+      validation: () => true,
+    },
+  ) {
+    if (!options.time) options.time = 30000;
+    else if (!options.max) options.max = 3;
+    else if (!options.validation) {
+      options.validation = () => true;
+    }
+
+    this.key = this.ctx.authorNumber + '-' + this.ctx.getCurrentJid();
+    this.validate = this.options.validation;
+  }
+
+  public events: EventEmitter = new EventEmitter();
+
+  /**
+   * Collected context
+   */
+  public contexts: Context[] = [];
+
+  /**
+   * Get the waiting time.
+   *
+   * @return {number}
+   */
+  public get time(): number {
+    return this.options.time as number;
+  }
+
+  /**
+   * Get the maximum messages want to receive.
+   *
+   * @return {number}
+   */
+  public get maxMessages(): number {
+    return this.options.max as number;
+  }
+
+  /**
+   * Start the message collector
+   *
+   * @return {void}
+   */
+  public start(): void {
+    if (this.runner) {
+      throw new Error('This instance is already started');
+    }
+
+    this.runner = setTimeout(() => {
+      this.destroy();
+    }, this.time);
+    this.ctx.client.collectors.set(this.key, this);
+  }
+
+  /**
+   * Destroy the message collector session.
+   *
+   * @return {void}
+   */
+  public destroy(): void {
+    if (this.runner) {
+      clearTimeout(this.runner);
+      this.runner = undefined;
+    }
+    if (this.ctx.client.collectors.has(this.key)) {
+      this.ctx.client.collectors.delete(this.key);
+    }
+    this.events.emit('end');
+  }
+
+  /**
+   * Wait message collector until 'end' event is fired.
+   */
+  public async wait(): Promise<boolean> {
+    return await new Promise((resolve) => {
+      this.events.on('end', () => resolve(true));
+    });
+  }
+}
