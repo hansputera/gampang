14b59eab4f39eb4c62c32102264be1be07770f2d
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-08-14 01:50:52 +0000

    fix(utils.qrHandler): fixed possibly undefined Object
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/utils/qrHandler.ts
+++ src/utils/qrHandler.ts
@@ -8,13 +8,13 @@ export const qrHandler = async (
   qr: string,
   qrOptions: ClientOptions['qr'],
 ): Promise<void> => {
-  if (qrOptions.store === 'file') {
-    if (typeof qrOptions.options?.dest !== 'string')
+  if (qrOptions?.store === 'file') {
+    if (typeof qrOptions?.options?.dest !== 'string')
       throw new TypeError('Please fill QR Path destination!');
-    qrcode.toFile(qrOptions.options.dest, qr, (err) => {
+    qrcode.toFile(qrOptions?.options.dest, qr, (err) => {
       if (err) console.error('Something was wrong:', err);
     });
-  } else if (qrOptions.store === 'web') {
+  } else if (qrOptions?.store === 'web') {
     if (!qrOptions.options?.port || typeof qrOptions.options?.port !== 'number')
       throw new TypeError('Please fill QR Server Port number');
 
@@ -35,7 +35,7 @@ export const qrHandler = async (
     });
 
     client.qrServer?.listen(port, '0.0.0.0');
-  } else if (qrOptions.store === 'terminal') {
+  } else if (qrOptions?.store === 'terminal') {
     console.log(await qrcode.toString(qr));
   }
   return;
