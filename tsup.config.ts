6869863336cca370533929f81f4ef864a737e7fd
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-10-16 03:23:00 +0000

    style: formatting tsconfig.json and tsup.config.ts
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
-import {defineConfig} from 'tsup';
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entry: ['src/index.ts'],
-    clean: true,
-    dts: true,
-    outDir: 'dist',
-    platform: 'node',
-    target: ['node14', 'node16'],
-    minify: true,
+  entry: ['src/index.ts'],
+  clean: true,
+  dts: true,
+  outDir: 'dist',
+  platform: 'node',
+  target: ['node14', 'node16'],
+  minify: true,
 });
