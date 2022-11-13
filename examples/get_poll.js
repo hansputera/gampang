486dfa1927f81f61095d441d64989ce7d333629b
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-11-13 06:28:16 +0000

    chore: update version
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

---
+++ examples/get_poll.js
@@ -0,0 +1,51 @@
+const { Client, SessionManager } = require('../dist/index.js');
+const path = require('node:path');
+
+const polls = {};
+
+const session = new SessionManager(
+  path.resolve(__dirname, 'sessions'),
+  'folder',
+);
+
+const client = new Client(session, {
+  'qr': {
+    'store': 'web',
+    'options': {
+      'port': 3000,
+    },
+  },
+  'prefixes': ['.'],
+});
+
+client.on('ready', () => {
+  console.log(client.raw?.user, 'ready');
+});
+
+client.on('poll', (poll) => (polls[poll.pollId] = poll));
+client.on('vote', async (vote, ctx) => {
+  const poll = polls[vote.pollId];
+
+  if (poll) {
+    console.log('vote', vote);
+    console.log('saved poll', poll);
+    console.log('raw message', ctx.raw);
+    const dec = await ctx.getPollUpdates(
+      ctx.raw,
+      {
+        encKey: poll.encKey,
+        sender: poll.sender,
+        options: poll.options,
+      },
+      true,
+    );
+
+    console.log(dec);
+  }
+});
+
+client.command('test-poll', async (ctx) => {
+  await ctx.createPoll('Poll Test', ['Apple', 'Orange']);
+});
+
+client.launch();
