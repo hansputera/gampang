1864373bb0dc83f9844ab82099148d39e296d6fa
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-11-13 06:30:49 +0000

    docs: ignore Core
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,8 @@ export * from './bot';
 export * from './@typings';
 export * from './structures';
 export * from './utils/sessionManager';
+/**
+ * See https://adiwajshing.github.io/Baileys/ for more information
+ * @ignore
+ */
 export * as Core from '@adiwajshing/baileys';
